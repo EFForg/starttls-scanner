71b1ff20e6587688e221fc7d73aee58af72c9013
Vivian Brown <vivian@eff.org>
2018-12-10 14:48:03 -0800

    Check mta-sts policy record

--- checker/mta_sts.go
+++ checker/mta_sts.go
@@ -2,12 +2,21 @@ package checker
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 // LookupTXT = net.LookupTXT
+
+type MTASTSResult struct {
+	Mode        string
+	MXHostnames []string
+	ResultGroup
+}
 
 func filterByPrefix(records []string, prefix string) []string {
 	filtered := []string{}
@@ -19,14 +28,21 @@ func filterByPrefix(records []string, prefix string) []string {
 	return filtered
 }
 
-func parseTXT(record string) map[string]string {
+func getKeyValuePairs(record string, lineDelimiter string,
+	pairDelimiter string) map[string]string {
 	parsed := make(map[string]string)
-	for _, line := range strings.Split(record, ";") {
-		split := strings.Split(strings.TrimSpace(line), "=")
+	for _, line := range strings.Split(record, lineDelimiter) {
+		split := strings.Split(strings.TrimSpace(line), pairDelimiter)
 		if len(split) != 2 {
 			continue
 		}
-		parsed[strings.TrimSpace(split[0])] = strings.TrimSpace(split[1])
+		key := strings.TrimSpace(split[0])
+		value := strings.TrimSpace(split[1])
+		if parsed[key] == "" {
+			parsed[key] = value
+		} else {
+			parsed[key] = parsed[key] + " " + value
+		}
 	}
 	return parsed
 }
@@ -38,7 +54,7 @@ func checkMTASTSRecord(records []string) CheckResult {
 	if len(records) != 1 {
 		return result.Failure("exactly 1 MTA-STS TXT record required, found %d", len(records))
 	}
-	record := parseTXT(records[0])
+	record := getKeyValuePairs(records[0], ";", "=")
 
 	id_re := regexp.MustCompile("^[a-zA-Z0-9]+$")
 	if !id_re.MatchString(record["id"]) {
@@ -47,19 +63,55 @@ func checkMTASTSRecord(records []string) CheckResult {
 	return result.Success()
 }
 
-// func checkTLSRPTRecord(records []string) error {
-// 	records = filterByPrefix(records, "v=STSv1")
-// 	if len(records) != 1 {
-// 		return fmt.Errorf("exactly 1 TLSRPT TXT record required, found %d", len(records))
-// 	}
-// 	record := parseTXT(records[0])
-// 	if record["v"] != "TLSRPTv1" {
-// 		return fmt.Errorf("TLSRPT TXT record version must be TLSRPTv1")
-// 	}
-// 	// @TODO validate record["rua"]
-// 	return nil
-// }
-//
+func checkMTASTSPolicyFile(domain string) CheckResult {
+	result := CheckResult{Name: "policy_file"}
+	policyURL := fmt.Sprintf("https://mta-sts.%s/.well-known/mta-sts.txt", domain)
+	resp, err := http.Get(policyURL)
+	// @TODO verify https? verify cert?
+	// @TODO don't follow redirect
+	// @TODO validate media type is 'text/plain'
+	if err != nil {
+		return result.Failure("Couldn't find policy file: %v", err)
+	}
+	if resp.StatusCode != 200 {
+		return result.Failure("Couldn't get policy file: %s", resp.Status)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return result.Error("Couldn't read policy file: %v", err)
+	}
+
+	policy := getKeyValuePairs(string(body), "\n", ":")
+
+	// Validate version
+	if policy["version"] != "STSv1" {
+		return result.Error("Policy version must be STSv1")
+	}
+
+	// Validate mode
+	// @TODO store the mode
+	if policy["mode"] == "" {
+		return result.Error("Policy file must specify mode")
+	}
+	if m := policy["mode"]; m != "enforce" && m != "testing" && m != "none" {
+		return result.Error("Mode must be one of 'enforce', 'testing', or 'none', got %s", m)
+	}
+
+	// Validate max age
+	if policy["max_age"] == "" {
+		return result.Error("Policy file must specify max_age")
+	}
+	if i, err := strconv.Atoi(policy["max_age"]); err != nil || i <= 0 || i > 31557600 {
+		return result.Error("max_age must be a positive integer <= 31557600")
+	}
+
+	// @TODO test with no mxs
+	// @TODO store the mxs
+	strings.Split(policy["mx"], " ")
+
+	return result.Success()
+}
 
 func checkMTASTS(domain string) ResultGroup {
 	result := ResultGroup{
@@ -68,9 +120,11 @@ func checkMTASTS(domain string) ResultGroup {
 	}
 	results, err := net.LookupTXT(fmt.Sprintf("_mta-sts.%s", domain))
 	if err != nil {
-		// @TODO return an failure, probably want to roll into check
+		// @TODO return a failure, probably want to roll into check
 		return result
 	}
 	result.addCheck(checkMTASTSRecord(results))
+	result.addCheck(checkMTASTSPolicyFile(domain))
+	// @TODO add a check to compare hostnames to those supplied by DNS
 	return result
 }
--- checker/mta_sts_test.go
+++ checker/mta_sts_test.go
@@ -5,23 +5,32 @@ import (
 	"testing"
 )
 
-func TestParseTXT(t *testing.T) {
+func TestGetKeyValuePairs(t *testing.T) {
 	tests := []struct {
 		txt  string
+		ld   string
+		pd   string
 		want map[string]string
 	}{
-		{"", map[string]string{}},
-		{"v=STSv1; foo;", map[string]string{
+		{"", ";", "=", map[string]string{}},
+		{"v=STSv1; foo;", ";", "=", map[string]string{
 			"v": "STSv1",
 		}},
-		{"v=STSv1; id=20171114T070707;", map[string]string{
+		{"v=STSv1; id=20171114T070707;", ";", "=", map[string]string{
 			"v":  "STSv1",
 			"id": "20171114T070707",
 		}},
+		{"version: STSv1\nmode: enforce\nmx: foo.example.com\nmx: bar.example.com\n\n", "\n", ":", map[string]string{
+			"version": "STSv1",
+			"mode":    "enforce",
+			"mx":      "foo.example.com bar.example.com",
+		}},
 	}
 	for _, test := range tests {
-		if got := parseTXT(test.txt); !reflect.DeepEqual(got, test.want) {
-			t.Errorf("parseTXT(%s) = %v, want %v", test.txt, got, test.want)
+		got := getKeyValuePairs(test.txt, test.ld, test.pd)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("getKeyValuePairs(%s, %s, %s) = %v, want %v",
+				test.txt, test.ld, test.pd, got, test.want)
 		}
 	}
 }
